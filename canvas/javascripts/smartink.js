77c09c7fa6ec9c4f162b98e0bf8156c685107063
unknown <hishin@mit.edu>
2017-01-18 13:59:41 -0500

    smart ink bounding box

--- canvas/javascripts/smartink.js
+++ canvas/javascripts/smartink.js
@@ -6,10 +6,12 @@ var sslide;
 var scanvas;
 var spaper;
 var SLIDE_W = 600;
-var SLIDE_H = 450;
+var SLIDE_H = 400;
+var numslides;
 
 window.onload = function () {
     setupSlideCanvas();
+    numslides = document.getElementById("slide-deck").length;
 };
 
 function setupSlideCanvas() {
@@ -45,15 +47,68 @@ function loadSlide() {
     spaper.project.activeLayer.importSVG(slide_src, {
         expandShapes: true,
         applyMatrix: true,
-        onLoad: function (svgitem, data) {
-            var wscale = parseFloat(spaper.canvas.width) / svgitem.bounds.width;
-            var hscale = parseFloat(spaper.canvas.height) / svgitem.bounds.height;
-            svgitem.scale(wscale, hscale);
-            var delta = new paper.Point(-svgitem.bounds.left, -svgitem.bounds.top);
-            svgitem.translate(delta);
-            showHiddenItems(svgitem);
+        onLoad: function (svgslide, data) {
+            var wscale = parseFloat(spaper.canvas.width) / svgslide.bounds.width;
+            var hscale = parseFloat(spaper.canvas.height) / svgslide.bounds.height;
+            svgslide.scale(wscale, hscale);
+            var delta = new paper.Point(-svgslide.bounds.left, -svgslide.bounds.top);
+            svgslide.translate(delta);
+            addItemBorders(svgslide);
         }
     });
+};
+
+function addItemBorders(svgslide) {
+    var item;
+    // Special case:: children[0] is always the slide background
+    for (var i = 1; i < svgslide.children.length; i++) {
+        item = svgslide.children[i];
+        if (!item.border) {
+            item.border = new paper.Path.Rectangle(item.bounds);
+            item.border.item = item;
+            item.border.strokeColor = '#3198C8';
+            item.border.strokeWidth = 2;
+            item.border.dashArray = [3,2];
+            item.border.opacity = 0.5;
+        }
+         if (!item.bbox) {
+             item.bbox = new paper.Shape.Rectangle(item.bounds);
+             item.bbox.item = item;
+             item.bbox.fillColor = '#000000';
+             item.bbox.opacity = 0;
+             item.bbox.onMouseEnter = function(event) {
+                 this.item.border.dashArray = [];
+             };
+             item.bbox.onMouseLeave = function(event) {
+                 this.item.border.dashArray = [3,2];
+             };
+             item.bbox.onClick = function(event) {
+                 console.log("click");
+             };
+        }
+
+
+    }
+};
+
+
+
+function showHiddenItems() {
+
+};
+
+function prevSlide() {
+    if ( document.getElementById('slide-deck').selectedIndex > 0) {
+        document.getElementById("slide-deck").selectedIndex--;
+        loadSlide();
+    }
+};
+
+function nextSlide() {
+    if (document.getElementById('slide-deck').selectedIndex < numslides - 1) {
+        document.getElementById("slide-deck").selectedIndex++;
+        loadSlide();
+    }
 };
 
 
